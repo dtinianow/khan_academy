ea5bb045d64907fed733324e8321e441d63f64fb
David Tinianow <david.tinianow@gmail.com>
2016-12-11 18:43:32 -0700

    Change increment starting point in insertion sort function

--- algorithms/insertion_sort.js
+++ algorithms/insertion_sort.js
@@ -1,13 +1,13 @@
 var insert = function (array, rightIndex, value) {
   for (var j = rightIndex; j >= 0 && array[j] > value; j--) {
-    array[j + 1] = array[j];
+    array[j + 1] = array[j]
   }
   array[j + 1] = value;
 };
 
 var insertionSort = function (array) {
-  for (var i = 0; i < array.length - 1; i++) {
-    insert(array, i, array[i + 1]);
+  for (var i = 1; i < array.length; i++) {
+    insert(array, i - 1, array[i]);
   }
 };
 
